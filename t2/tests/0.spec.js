1a9b731c0a0290756688ee9a621123d5a6df809d
任跃兵 <cnryb@live.cn>
2022-06-06 23:28:03 +0800

    t2

---
+++ t2/tests/0.spec.js
@@ -0,0 +1,31 @@
+const { test, expect } = require('@playwright/test');
+
+test('性别字典数据正确', async ({ page, request }) => {
+  const response = await request.post('https://fe.zhaopin.com/data-normalization/api/general', { data: [{ "dictName": "sex" }] })
+  // console.log((await response.body()).toString())
+  expect(response.ok()).toBeTruthy();
+  const obj = {
+    "code": 200,
+    "message": "success",
+    "data": [
+      {
+        "config": {
+          "dictName": "sex"
+        },
+        "data": [
+          {
+            "value": 1,
+            "label": "男",
+            "extra": {}
+          },
+          {
+            "value": 2,
+            "label": "女",
+            "extra": {}
+          }
+        ]
+      }
+    ]
+  }
+  expect(await response.json()).toMatchObject((obj));
+});
